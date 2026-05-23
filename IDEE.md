f00e46fef462c2b6f7b73092faf7fee6236ed8d4
NasaEnder <natis.saillet@gmail.com>
2026-05-23 15:11:37 +0200

    test

---
+++ IDEE.md
@@ -0,0 +1,16 @@
+entrée sutr le site : 
+
+- landing page "Entrer"
+
+- Page "Menu"
+
+- page commander a part
+
+ - animation sur Card Bijoux
+
+ - prise nde rendez vous au lieu de commander piece
+
+ Idée Landing page : https://www.awwwards.com/sites/detroit-paris
+ ecrit "Focus" en spirale avec fondu au centre et "Entrer" Ecrit
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getJewels } from '@/lib/supabase'
+import type { Jewel } from '@/types'
 import Hero from '@/components/sections/Hero'
 import Gallery from '@/components/sections/Gallery'
 import About from '@/components/sections/About'
@@ -6,7 +7,7 @@ import Contact from '@/components/sections/Contact'
 import Footer from '@/components/sections/Footer'
 
 export default async function Home() {
-  let jewels = []
+  let jewels: Jewel[] = []
   try {
     jewels = await getJewels()
   } catch (err) {
